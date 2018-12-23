055d18b82c0591fdfe5335d401010b83aae966ef
Tomer Koren <tomer@klear.com>
2018-12-23 16:59:44 +0200

    Add a couple more examples

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -6,14 +6,16 @@ export interface PayloadAction<T extends string, P> extends TypedAction<T> {
   payload: P;
 }
 
+export type Action<T extends string = string, P = any> = TypedAction<T> | PayloadAction<T, P>
+
 export type State = number[];
 
 /**
  * Conditionals with generics only decide types at time of type checking, so unfortunately cannot use as method output here.
  */
-function createAction<T extends string>(type: T): TypedAction<T>;
-function createAction<T extends string, P>(type: T, payload: P): PayloadAction<T, P>;
-function createAction<T extends string, P = undefined>(type: T, payload?: P): TypedAction<T> | PayloadAction<T, P> {
+export function createAction<T extends string>(type: T): TypedAction<T>;
+export function createAction<T extends string, P>(type: T, payload: P): PayloadAction<T, P>;
+export function createAction<T extends string, P = undefined>(type: T, payload?: P): TypedAction<T> | PayloadAction<T, P> {
   return payload === undefined ? {type} : {type, payload};
 }
 
@@ -29,3 +31,5 @@ export const clearAction = () => createAction(ActionsTypes.Clear);
 export const addAction = (item: number) => createAction(ActionsTypes.Add, item);
 
 export type Actions = ReturnType<typeof setAction> | ReturnType<typeof clearAction> | ReturnType<typeof addAction>;
+
+
--- src/app/reducer.ts
+++ src/app/reducer.ts
@@ -1,4 +1,4 @@
-import {Actions, ActionsTypes, State} from './actions';
+import {Action, Actions, ActionsTypes, createAction, State} from './actions';
 
 function reducer(state: State, action: Actions): typeof state {
   switch (action.type) {
@@ -13,3 +13,17 @@ function reducer(state: State, action: Actions): typeof state {
       return state;
   }
 }
+
+/**
+ * The typing on this thing causes narrowing of the action input to it to narrow correctly.
+ * Only works if action is previously narrowed to union type of actions  (A1 | A2 | ...)
+ */
+export function ofType<T extends string = string>(...types: T[]) {
+  return <A extends Action, U extends Extract<A, { type: T }>>(action: A): action is U =>
+    types.some(type => action.type === type);
+}
+
+let actions = [createAction(ActionsTypes.Set, [1,2,3]), createAction(ActionsTypes.Add, 3),
+      createAction(ActionsTypes.Clear)];
+
+let setActions = actions.filter(ofType(ActionsTypes.Set));
